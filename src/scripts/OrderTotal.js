5f77307009c80cbe85aa6a83a009ceed3ee29f4f
Julian Muratori <julianmuratori@gmail.com>
2017-05-21 16:28:25 -0400

    idk

--- src/scripts/OrderTotal.js
+++ src/scripts/OrderTotal.js
@@ -4,9 +4,16 @@ import React from 'react';
 class OrderTotal extends React.Component {
 
   render() {
+  	const order = this.props.orderItems;
+  	const total = () => {
+  		let cost = 0;
+  		// Object.keys(order)
+  		// 		.map(key => console.log("hi"))
+  		console.log("hi")
+  	}
     return (
       <div>
-        <h5>yo</h5>
+        <h5>{this.total()}</h5>
       </div>
     )
   }
